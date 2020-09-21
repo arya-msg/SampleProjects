2f9b62d19d22eba6c5f817cc5aaaa2397c4e01f1
arya-msg <a.m@sample.com>
2020-09-21 14:29:56 +0530

    Added ReadME File and modified promotion engine service test by adding the test scenarios and test cases

--- PromotionEngine/src/com/ecom/promotionengine/service/test/PromotionEngineServiceTest.java
+++ PromotionEngine/src/com/ecom/promotionengine/service/test/PromotionEngineServiceTest.java
@@ -1,7 +1,5 @@
 package com.ecom.promotionengine.service.test;
 
-import static org.junit.Assert.*;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -12,6 +10,10 @@ public class PromotionEngineServiceTest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		/*
+		 * Test setup Unit price for SKU IDss A 50 B 30 C 20 D 15 
+		 * Active Promotions :  3 of A's for 130 2 of B's for 45 C & D for 30
+		 */
 	}
 
 	@AfterClass
@@ -27,8 +29,18 @@ public class PromotionEngineServiceTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testPromotionEngineScenarioA() {
+	// Scenario B : 1 * A 50 1 * B 30 1 * C 20 ====== Total 100
+	}
+	
+	@Test
+	public void testPromotionEngineScenarioB() {
+	// Scenario B : 5 * A 130 + 2*50 5 * B 45 + 45 + 30 1 * C 20 ====== Total 370
+	}
+	
+	@Test
+	public void testPromotionEngineScenarioC() {
+	// Scenario C : 3 * A 130 5 * B 45 + 45 + 1 * 30 1 * C - 1 * D 30 ====== Total 280
 	}
 
 }
