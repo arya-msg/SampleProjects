bc953efb0e35bc3436a30f327e38fd3a72332b3e
arya-msg <a.m@sample.com>
2020-09-21 14:50:50 +0530

    Added implementation class for promotion engine

---
+++ PromotionEngine/src/com/ecom/promotionengine/service/impl/PromotionServiceImpl.java
@@ -0,0 +1,14 @@
+package com.ecom.promotionengine.service.impl;
+
+import com.ecom.promotionengine.service.PromotionService;
+import com.ecom.promotionengine.service.domain.Cart;
+
+public class PromotionServiceImpl implements PromotionService {
+
+	@Override
+	public double applyPromotion(Cart cart) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
