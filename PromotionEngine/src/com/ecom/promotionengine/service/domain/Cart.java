d8e2e3d92c2514b66da1a3a03d40894e8d75f774
arya-msg <a.m@sample.com>
2020-09-21 14:59:02 +0530

    Added Cart corresponding to the problem statement Our Cart contains a list of single character SKU ids (A, B, C....) over which the promotion engine will need to run.

--- PromotionEngine/src/com/ecom/promotionengine/service/domain/Cart.java
+++ PromotionEngine/src/com/ecom/promotionengine/service/domain/Cart.java
@@ -1,6 +1,7 @@
 package com.ecom.promotionengine.service.domain;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Cart implements Serializable{
 
@@ -9,4 +10,25 @@ public class Cart implements Serializable{
 	 */
 	private static final long serialVersionUID = 3205007495437507242L;
 
+	private List<Character> skuList;
+
+	/**
+	 * @return the skuList
+	 */
+	public List<Character> getSkuList() {
+		return skuList;
+	}
+
+	/**
+	 * @param skuList the skuList to set
+	 */
+	public void setSkuList(List<Character> skuList) {
+		this.skuList = skuList;
+	}
+
+	@Override
+	public String toString() {
+		return "Cart [skuList=" + skuList + "]";
+	}
+	
 }
